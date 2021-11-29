8428dd2851726df51a306144a5d77fa628ad62bc
iamrahulrnair <rahulrnair007@outlook.com>
2021-11-29 08:18:53 +0530

    update listener

--- posting/src/events/listeners/review-created-listener.ts
+++ posting/src/events/listeners/review-created-listener.ts
@@ -1,22 +1,32 @@
-import { Listener, ReviewCreatedEvent, Subjects } from '@suup/common';
+import {
+  BadRequestError,
+  Listener,
+  ReviewCreatedEvent,
+  Subjects,
+} from '@suup/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
 import { Review } from '../../models/review';
+import { Post } from '../../models/post';
 
 export class ReviewCreatedListener extends Listener<ReviewCreatedEvent> {
   readonly subject: Subjects.reviewCreated = Subjects.reviewCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: ReviewCreatedEvent['data'], msg: Message) {
-    const { id, userId, postId, title, description, rating } = data;
-    const review = await Review.build({
-      id,
-      userId,
-      postId,
-      title,
-      description,
-      rating,
-    });
-    await review.save();
-    msg.ack();
+    try {
+      const { id, userId, postId, title, description, rating } = data;
+      const review = await Review.build({
+        id,
+        userId,
+        postId,
+        title,
+        description,
+        rating,
+      });
+      await review.save();
+      msg.ack();
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
